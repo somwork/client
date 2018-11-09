c74bd22ca4530824d7e5c36931b207b02e05cf30
GGkarma <32382976+GGkarma@users.noreply.github.com>
2018-11-09 11:33:31 +0100

    Stash pop conflicts resolved

--- src/Routes.js
+++ src/Routes.js
@@ -3,9 +3,13 @@ import { Route } from "react-router-dom";
 import Main from './views/Main';
 import Second from './views/Second';
 import Create from './views/Task/Create';
+import Update from './views/Task/Update';
+
 
 export default () => [
   <Route path='/' exact component={Main} />,
   <Route path='/second' component={Second} />,
   <Route path='/task/create' component={Create} />,
+  <Route path='/task/update' component={Update} />,
 ]
+
