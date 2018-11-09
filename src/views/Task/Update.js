faf61d52ee62b7a3fa2068ea452014692363e671
GGkarma <32382976+GGkarma@users.noreply.github.com>
2018-11-09 13:54:52 +0100

    Task management update (#16)
    * Package-lock.json update
    
    * Added package for TaskManagement in views + index.js file
    
    * Adjusted package structure
    
    * Stash pop conflicts resolved
    
    * Typos
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/Routes.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Update src/views/Task/Update.js
    
    Co-Authored-By: GGkarma <32382976+GGkarma@users.noreply.github.com>
    
    * Format
    
    * Format

---
+++ src/views/Task/Update.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout>
+    <h2>Edit Task</h2>
+    <label>
+      Title
+      <input type="text" />
+    </label>
+    <label>
+      Start Date
+      <input type="date" />
+    </label>
+    <label>
+      Deadline
+      <input type="date" />
+    </label>
+    <label>
+      Description
+      <textarea />
+    </label>
+    <label>
+      Urgency
+      <input type="text" />
+    </label>
+    <input type="submit" value="Save Changes" />
+  </Layout>
+)
