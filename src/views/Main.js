736089d7d89ce067c4068e87e214c868402ad4f6
alex6296 <32260519+alex6296@users.noreply.github.com>
2018-11-09 10:48:21 +0100

    -added validation & added post request
    -added validation for input
    -added a way to send a post to the server

--- src/views/Main.js
+++ src/views/Main.js
@@ -6,8 +6,8 @@ export default () => (
   <Layout>
     <section>
       <h1>Main View</h1>
-      <Link to='/second'>Second view</Link>
-      <Link to='/sign-up'>Sing up</Link>
+      <p> <Link to='/second'>Second view</Link></p>
+      <p> <Link to='/sign-up'>Sing up</Link></p>
     </section>
   </Layout>
 )
--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -13,29 +13,92 @@ export default class SignUp extends Component{
       password:''
     };
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmitFields = this.handleSubmitFields.bind(this);
+    this.VerifyString = this.VerifyString.bind(this);
+    this.VerifyEmail = this.VerifyEmail.bind(this);
     this.verifyPassword = this.verifyPassword.bind(this);
+    this.comparePassword = this.comparePassword.bind(this);
   }
 
-  handleChange({ target }) {
+  handleSubmitFields(event) {
+
+    if(this.state.includes(null)){
+      return;
+    }
+
+    var url = 'https://localhost:5001/api/workers/';
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+    event.preventDefault();
+  }
+
+  VerifyString({ target }) {
     const {name} = target;
+    if(name.length<=0){
+      console.log('is empty')
+      return;
+    }
+    console.log(name+' exsists')
 
     this.setState({
       [name]: target.value
     });
   }
 
-  handleSubmitFields(event) {
+  VerifyEmail({ target }) {
+    const {name} = target;
+    if(!target.value.includes('@')){
+      console.log('email needs @')
+      return;
+    }
+    console.log('email contains @')
 
-    event.preventDefault();
+    if(!target.value.includes('.')){
+      console.log('email does not contain dot( . )')
+      return;
+    }
+    console.log('email contaions dot( . )')
+
+      this.setState({
+        [name]: target.value
+      });
+
   }
 
-  verifyPassword(event) {
-    if(this.state.password===event.target.value) {
-      console.log('password valid')
-      alert('Passwords match')
+  verifyPassword({ target }) {
+    const {name} = target;
+
+    if(target.value.length<8) {
+      console.log('password to short')
+      return;
     }
+    console.log('password length ok')
+
+    this.setState({
+      [name]: target.value
+    });
+  }
+
+  comparePassword({ target }) {
+    const {name} = target;
+
+    if(this.state.password!==target.value) {
+      console.log('password do not match')
+      return;
+    }
+    console.log('password match')
+
+    this.setState({
+      [name]: target.value
+    });
   }
 
   render(){
@@ -45,32 +108,32 @@ export default class SignUp extends Component{
           <form onSubmit={this.handleSubmitFields}>
             <label>
               First Name
-              <input type="text" name="firstname" onChange={this.handleChange} required/>
+              <input type="text" name="firstname" onChange={this.VerifyString} required/>
             </label>
 
             <label>
               Last Name
-              <input type="text" name="lastname" onChange={this.handleChange} required/>
+              <input type="text" name="lastname" onChange={this.VerifyString} required/>
             </label>
 
             <label>
               Email
-              <input type="email" name="email" onChange={this.handleChange} required/>
+              <input type="email" name="email" onChange={this.VerifyEmail} required/>
             </label>
 
             <label>
               Username
-              <input type="username" name="username" onChange={this.handleChange} required/>
+              <input type="username" name="username" onChange={this.VerifyString} required/>
             </label>
 
              <label>
               Password
-              <input type="password" name="password" min="8" onChange={this.handleChange} required/>
+              <input type="password" name="password" min="8" onChange={this.verifyPassword} required/>
             </label>
 
             <label>
              Verify Password
-              <input type="password" name="verifyPassword"  min="8" onChange={this.verifyPassword} required/>
+              <input type="password" name="verifyPassword"  min="8" onChange={this.comparePassword} required/>
             </label>
 
             <label>
