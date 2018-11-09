a4b3ecbb618af5422370bec729b0dede14e7e833
grammeaway <victor_gram@ymail.com>
2018-11-09 10:45:50 +0100

    Input fields created

--- src/views/Task/Create.js
+++ src/views/Task/Create.js
@@ -4,6 +4,27 @@ import Layout from '../../components/Layout'
 export default ({ children }) => (
 
   <Layout>
+    <label>
+      Task Title
+      <input type="text" />
+    </label>
+    <label>
+      Starting Date of Task
+      <input type="date" />
+    </label>
+    <label>
+      Task Deadline
+      <input type="date" />
+    </label>
+    <label>
+      Task Description
+      <input type="text" />
+    </label>
+    <label>
+      Urgency
+      <input type="text" />
+    </label>
+    <input type="submit" />
 
 
   </Layout>
