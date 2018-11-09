7cc762f69e0bb21c39f30deea706ddaac86271b8
GGkarma <32382976+GGkarma@users.noreply.github.com>
2018-11-09 13:50:53 +0100

    Format

--- src/views/Task/Update.js
+++ src/views/Task/Update.js
@@ -3,27 +3,27 @@ import Layout from '../../components/Layout'
 
 export default () => (
   <Layout>
-      <h2>Edit Task</h2>
-      <label>
-         Title    
+    <h2>Edit Task</h2>
+    <label>
+      Title
          <input type="text" />
-      </label>
-      <label>
-         Start Date 
+    </label>
+    <label>
+      Start Date
          <input type="date" />
-      </label>
-      <label>
-         Deadline
-        <input type="date"/>
-      </label>
-      <label>
-         Description    
-         <textarea/>
-     </label>
-     <label>
-         Urgency    
-         <input type="text"/>
-     </label>
-     <input type="submit" value="Save Changes"/>        
-	</Layout>
+    </label>
+    <label>
+      Deadline
+        <input type="date" />
+    </label>
+    <label>
+      Description
+         <textarea />
+    </label>
+    <label>
+      Urgency
+         <input type="text" />
+    </label>
+    <input type="submit" value="Save Changes" />
+  </Layout>
 )
