03145fedd769451e7ea989e71b5256fdd761894c
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-11-11 10:38:57 +0100

    v1

--- src/Routes.js
+++ src/Routes.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import Main from './views/Main';
 import Second from './views/Second';
+import Login from './views/Login';
 
 export default () => [
   <Route path='/' exact component={Main} />,
   <Route path='/second' component={Second} />,
+  <Route path='/login' component={Login} />
 ]
---
+++ src/views/Login/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Layout from "../../components/Layout";
+import "./Login.css";
+
+export default class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleLogin = async event => {
+    event.preventDefault();
+    await fetch("https://localhost:5001/api/token", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        localStorage.setItem("Tokens", JSON.stringify(myJson));
+        console.log(JSON.stringify(myJson));
+      });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <section>
+          <h1>Login</h1>
+          <form onSubmit={this.handleLogin}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Login" />
+          </form>
+          <Link to="/">Main view</Link>
+        </section>
+      </Layout>
+    );
+  }
+}
