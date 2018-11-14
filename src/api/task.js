542655ae12c6b2194c2f4d4207a7ee0006ff7ee3
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-11-14 09:56:17 +0100

    Changed Create.js to class, added methods

--- src/api/task.js
+++ src/api/task.js
@@ -12,7 +12,7 @@ export default {
 
   /**
    * Gets specifik task based on task id
-   * @param {*} id
+   * @param {Number} id
    */
   async get(id) {
     const res = await request.get(`tasks/${id}`)
@@ -21,16 +21,16 @@ export default {
 
   /**
    * Create new Task from Json body
-   * @param {*} body
+   * @param {Object} body
    */
   async create(body) {
-    const res = await request.post('tasks', body.json())
+    const res = await request.post('tasks', body)
     return await res.json()
   },
 
   /**
    * Update existing Task, based on Json body
-   * @param {*} body
+   * @param {Object} body
    */
   async update(body) {
     const res = await request.update('tasks', body.json)
@@ -39,7 +39,7 @@ export default {
 
   /**
    * Delete exisintg task through id
-   * @param {*} id
+   * @param {Number} id
    */
   async delete(id) {
     const res = await request.delete(`tasks/${id}`)
--- src/views/Task/Create.js
+++ src/views/Task/Create.js
@@ -1,30 +1,63 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Layout from '../../components/Layout'
 import task from '../../api/task'
 
-export default ({ children }) => (
-  <Layout>
-    <h2>Create Task</h2>
-    <label>
-      Title
+export default class Create extends Component {
+
+  createTaskOnClick = () => {
+   // validateInput()
+   this.getCurrentDateString()
+  }
+
+
+  validateInput() {
+
+  }
+
+  getCurrentDateString = () => {
+    const date = new Date()
+
+    const year = date.getFullYear().toString()
+    const dash = "-"
+    const month = (date.getMonth() + 1).toString()
+    const day = date.getDate().toString()
+
+    let dateString = year.concat(dash, month, dash, day)
+
+    console.log(dateString)
+
+    return dateString
+  }
+
+
+
+
+  render() {
+    return (
+      <Layout>
+        <h2>Create Task</h2>
+        <label>
+          Title
       <input type="text" />
-    </label>
-    <label>
-      Starting Date
-      <input type="date" />
-    </label>
-    <label>
-      Deadline
-      <input type="date" />
-    </label>
-    <label>
-      Description
+        </label>
+        <label>
+          Starting Date
+      <input type="date" min={this.getCurrentDateString()}/>
+        </label>
+        <label>
+          Deadline
+      <input type="date"  />
+        </label>
+        <label>
+          Description
       <textarea></textarea>
-    </label>
-    <label>
-      Urgency
+        </label>
+        <label>
+          Urgency
       <input type="text" />
-    </label>
-    <input type="submit" value="Create Task" />
-  </Layout>
-)
+        </label>
+        <input type="submit" value="Create Task"  onClick={this.createTaskOnClick}/>
+      </Layout>
+    )
+  }
+}
