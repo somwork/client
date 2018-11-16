922c8875af8284c29934892734e033c17a6d7a8c
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-11-16 13:18:03 +0100

    Creates succesfully

--- src/views/Task/Create.js
+++ src/views/Task/Create.js
@@ -7,26 +7,61 @@ import moment from 'moment'
 export default class Create extends Component {
 
   state = {
-    startDate : moment(),
-    endDate : null
+    startDate: moment(),
+    endDate: moment(),
+    title: null,
+    description: null,
+    urgency: null
   }
 
 
+  /**
+   * Creates task based on data in inputfields
+   */
   createTaskOnClick = () => {
-    // validateInput()
-    this.getCurrentDateString()
+      const taskData = {
+        title: this.state.title,
+        description: this.state.description,
+        urgency: this.state.urgency,
+        startDate: this.state.startDate,
+        endDate: this.state.endDate
+      }
+
+      task.create(taskData)
+      //Redirect to users task-overview
   }
+
+  /**
+   * Validate data in inputfields
+   */
 
   validateInput() {
+    //TODO
 
   }
 
+  /**
+   * Sets the selected  date
+   */
   updateStartDate = (date) => {
-    this.setState({startDate : date})
+    this.setState({ startDate: date })
   }
 
+  /**
+   * Sets selected date of deadline
+   */
   updateEndDate = (date) => {
-    this.setState({endDate : date})
+    this.setState({ endDate: date })
+
+  }
+
+  /**
+   * Sets state data when changes are made in text-inputs
+   */
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
 
   }
 
@@ -36,11 +71,11 @@ export default class Create extends Component {
         <h2>Create Task</h2>
         <label>
           Title
-      <input type="text" />
+      <input type="text" onChange={this.handleChange} name="title" />
         </label>
         <label>
           Starting Date
-      <DatePicker  onChange={this.updateStartDate} minDate={moment()} selected={this.state.startDate} />
+      <DatePicker onChange={this.updateStartDate} minDate={moment()} selected={this.state.startDate} />
         </label>
         <label>
           Deadline
@@ -48,11 +83,11 @@ export default class Create extends Component {
         </label>
         <label>
           Description
-      <textarea></textarea>
+      <textarea onChange={this.handleChange} name="description"></textarea>
         </label>
         <label>
           Urgency
-      <input type="text" />
+      <input type="text" onChange={this.handleChange} name="urgency" />
         </label>
         <input type="submit" value="Create Task" onClick={this.createTaskOnClick} />
       </Layout>
