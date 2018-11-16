11f74180458861d6915d6fe5f17a35c918815ba3
Niclas Johansen <32328015+NJohansen@users.noreply.github.com>
2018-11-16 14:48:24 +0100

    API for worker with CRUD functions (#33)
    * Worker API for create added
    
    * Rest api for worker created
    
    * docs added for function
    
    * fixed wrong commits
    
    * Requested changes made
    
    * indent added
    
    * Needs validate
    
    * Validation added for create and update
    
    * Simplified get function
    
    * fix property naming

--- src/api/worker.js
+++ src/api/worker.js
@@ -1,17 +1,58 @@
 import request from './request'
+import { struct } from 'superstruct'
+
+const Worker = struct({
+  id: 'number',
+  username: 'string',
+  password: 'string',
+  email: 'string',
+  firstName: 'string',
+  lastName: 'string',
+})
 
 export default {
   /**
-   * Get list of all workers
+   * Get worker and if id is undefined you get all workers
+   * @param {Number} id
    * @return {Promise}
    */
-  async get() {
-    const res = await request.get('workers');
-    return await res.json();
+  async get(id) {
+    const res = await request.get(id ? `workers/${id}` : 'workers')
+    return await res.json()
   },
-  async create (body){
-   const res = await request.post('workers', body);
-   return await res.json();
+
+  /**
+   * Create worker
+   * @param {Object} worker
+   * @return {Promise}
+   */
+  async create(worker) {
+      //Validate data for the body
+      Worker(worker);
+      const res = await request.post('workers', worker);
+      return await res.json()
+  },
+
+  /**
+   * Update worker
+   * @param {Object} worker
+   * @return {Promise}
+   */
+  async update(worker) {
+    //Validate data for the body
+    Worker(worker);
+    const res = await request.put(`workers/${worker.id}`, worker);
+    return res.ok
+  },
+
+  /**
+   * Delete worker
+   * @param {Number} id
+   * @return {Promise}
+   */
+  async delete(id) {
+     const res = await request.delete(`workers/${id}`);
+     return res.ok
   }
 }
 
