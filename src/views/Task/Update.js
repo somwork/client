549c3352b59721962cce26f685fcaa7dd624a0bc
GGkarma <32382976+GGkarma@users.noreply.github.com>
2018-11-20 11:52:02 +0100

    Update Update.js

--- src/views/Task/Update.js
+++ src/views/Task/Update.js
@@ -1,38 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Layout from '../../components/Layout'
 import task from '../../api/task'
 import DatePicker from '../../components/DatePicker'
 import moment from 'moment'
 
-export default class Update extends Comment {
+export default class Update extends Component {
+  state = {
+    startDate: moment(),
+    endDate: moment(),
+    title: "",
+    description: "",
+    urgency: "",
+    error: null
+  }
+  constructor(props) {
+    super(props);
 
+  };
+
+  componentDidMount = () => {
+
+  }
+
+  fields = [
+    //["label", "Type", Validation method]
+    ["Title", "text", v => v.length > 0, false],
+    ["Description", "text", v => v.length > 0, true],
+    ["Urgency", "text", v => v.length > 0, false]
+  ]
+
+
+
+  updateHandler = async () => {
+
+    try {
+      const taskData = {
+        title: this.state.title,
+        description: this.state.description,
+        urgency: this.state.urgency,
+        start: this.state.startDate.toDate(), //toDate() to convert moment()-date to standard JS, due to Superstruckt limitations
+        deadline: this.state.endDate.toDate() //toDate() to convert moment()-date to standard JS, due to Superstruckt limitations
+      }
+      await task.update(taskData)
+      //Redirect to employers task-overview
+    } catch (e) {
+      this.setState({ error: e.message })
+    }
+
+  }
 
   render() {
     return (
       <Layout>
-        <h2>Edit Task</h2>
-        <label>
-          Title
-      <input type="text" />
-        </label>
-        <label>
-          Start Date
-      <input type="date" />
-        </label>
-        <label>
-          Deadline
-      <input type="date" />
-        </label>
-        <label>
-          Description
-      <textarea />
-        </label>
-        <label>
-          Urgency
-      <input type="text" />
-        </label>
-        <input type="submit" value="Save Changes" />
+        <section>
+          <h2>Task</h2>
+          {this.state.error && (
+            <Alert>{this.state.error} </Alert>
+          )}
+          <form onSubmit={this.submitHandler}>
+            {this.fields.map(this.fieldRender.bind(this))}
+            <label>
+              Starting Date
+            <DatePicker onChange={this.updateStartDate} minDate={moment()} selected={this.state.startDate} />
+            </label>
+            <label>
+              Deadline
+            <DatePicker onChange={this.updateEndDate} minDate={this.state.startDate} selected={this.state.endDate} />
+            </label>
+          </form>
+          <input type="submit" value="Update" onClick={this.submitHandler} />
+        </section>
       </Layout>
     )
   }
+
 }
