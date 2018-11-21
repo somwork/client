e44c417fa9523f96d449318d8e2e47bf06c3f6c8
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-11-21 22:40:08 +0100

    check in user table

--- src/api/employer.js
+++ src/api/employer.js
@@ -27,10 +27,11 @@ export default {
    * @return {Promise}
    */
   async create(employer) {
-      //Validate data for the body
-      Employer(employer);
-      const res = await request.post('employers', employer);
-      return await res.json()
+    //Validate data for the body
+    Employer(employer);
+    const path = 'employers?password=' + employer.password
+    const res = await request.post(path, employer);
+    return await res.json()
   },
 
   /**
@@ -51,7 +52,7 @@ export default {
    * @return {Promise}
    */
   async delete(id) {
-     const res = await request.delete(`employers/${id}`);
-     return res.ok
+    const res = await request.delete(`employers/${id}`);
+    return res.ok
   }
 }
