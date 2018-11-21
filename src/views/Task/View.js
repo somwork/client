ba859238bffe034e7876e48aa3f239454d362779
alex6296 <32260519+alex6296@users.noreply.github.com>
2018-11-21 13:36:14 +0100

    added alot of stuff
    -chat
    -and moved stuff around
    -and more css

--- src/views/Task/View.js
+++ src/views/Task/View.js
@@ -4,6 +4,7 @@ import Task from '../../api/task';
 import Offer from '../../api/offer';
 import { Link } from 'react-router-dom';
 import Popup from "reactjs-popup";
+import'./TaskView.css'
 
 export default class View extends Component{
 
@@ -24,8 +25,35 @@ export default class View extends Component{
         currency:'',
         workerId:'',
         taskId:''
-      }]
+      }],
+      mesaggeInput:[{
+        sendtAt:'',
+        text:''
+      }],
+      message:[["11:11:11","asdASDAS"],["11:11:11","asdASDAS"],["11:11:11","asdASDAS"]],
+
+      error: null
     }
+
+    this.loadTasks(this.props.match.params.id);
+  }
+
+    /**
+   *loads the selected task from the db into the state
+   * @param {int} id
+   */
+  loadTasks = async id=>{
+    const res = await Task.get(id);
+
+    this.setState({tasks:res})
+  }
+
+  /**
+   * loads all messages from the db into the state
+   */
+  loadChat = async ()=>{
+    const res =await Task.getChat(); //TODO
+    this.setState({message:res})
   }
 
   /**
@@ -44,7 +72,7 @@ export default class View extends Component{
    * validates all inputfields before sending a post request to the server
    * @param {Object} event
    */
-  submitHandler= async event =>{
+  offerSubmitHandler= async event =>{
     event.preventDefault();
 
     if(!this.state.offer.price>0){
@@ -57,7 +85,7 @@ export default class View extends Component{
         accepted: '',
         price: this.state.offer.price,
         currency: '', //TODO
-        workerId: '',//TOD
+        workerId: '',//TODO
         taskId: this.state.Task.id,
       });
 
@@ -70,14 +98,27 @@ export default class View extends Component{
     }
   }
 
-  /**
-   *loads all tasks from the db into the state
-   * @param {int} id
-   */
-  loadTasks = async id=>{
-    const res = await Task.get(id);
+  mesaggeSubmithandler = async event =>{
+    event.preventDefault();
 
-    this.setState({tasks:res})
+    if(!this.state){
+      this.setState({ error: "Invalid input" })
+      return
+    }
+
+    try {
+      const res = await Task.createMessage({//TODO
+        sendtAt: this.mesaggeInput.sendtAt,
+        text: this.mesaggeInput.text
+      });
+
+      if (res.error) {
+        return this.setState({ error: res.error })
+      }
+
+    } catch(err) {
+      this.setState({ error: err.message })
+    }
   }
 
   /**
@@ -85,18 +126,55 @@ export default class View extends Component{
    * @param {Object} task
    * @return {JSX} a task as a list item
    */
- fieldRender(task){
+  fieldRenderTaskDescription(task){
     return(
-      <li key ={task.id}>
+      <label key ={task.id}>
+        <div class="flex-container">
+          <div>
+            <b>Task Id</b>
+            <div>
+            {task.id} !2345!
+            </div>
+          </div>
+          <div>
+            <b>published</b>
+            <div>
+            {task.start} 11:11:12
+            </div>
+          </div>
+          <div>
+            <b>Deadline</b>
+            <div>
+            {task.deadline} 99:99:99
+            </div>
+          </div>
+          <div>
+            <b>Urgency</b>
+            <div >
+            {task.urgency}! FIRE!
+            </div>
+          </div>
+          </div>
+          <div>
+          <hr></hr>
+          <p><h6>Task Description:</h6></p>
+          <p>{task.description}</p> <p>TEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST DATATEST </p>
+          </div>
+      </label>
+    )
+  }
+
+  fieldRenderChat(message){
+    return(
+      <li class="chatBox">
         <label>
-          <p><b>Task Id:</b> {task.id}</p>
-          <p>{task.start} → {task.deadline}</p>
-          <p>{task.urgency}</p>
-          <p>{task.description}</p>
+          <p>sendt at: {message.sendtAt}</p>
+          <p> TEMP CHAT TEXT BLA BLA BLA{message.text}</p>
         </label>
       </li>
     )
   }
+
 
   /**
    * Creates the Task overview view
@@ -106,24 +184,28 @@ export default class View extends Component{
     return (
       <Layout>
         <section>
-          <h1>Task view</h1>
+          <h1>Title Task</h1>
+          <h3>Taks Details</h3>
+          <hr></hr>
+          {this.fieldRenderTaskDescription(this.state.Task)}
+
+          <Popup trigger={<button> Make offer</button>}>
+            <div>
+              <label>
+                <input name='price:' type='number' onChange={this.changeHandler}  placeholder=" Estimated Hours" required/>
+                <input type="submit" value="Submit" submitHandler="offerSubmitHandler"/>
+              </label>
+            </div>
+          </Popup>
+          <hr></hr>
+
           <ul>
-            {this.fieldRender(this.state.Task)}
+          {this.state.message.map(this.fieldRenderChat.bind(this))}
           </ul>
-            <Popup trigger={<button> Make offer</button>}>
-              <div>
-                <label>
-                  <input
-                    name='price:'
-                    type='number'
-                    onChange={this.changeHandler}
-                    required
-                  />
-                  <input type="submit" value="Submit"/>
-                </label>
-              </div>
-            </Popup>
-          <button>Chat</button>
+          <div class="chat">
+          <input name='text' type='text' onChange={this.changeHandler}  placeholder="enter your massage..."/>
+          <input type="submit" value="send" submitHandler="mesaggeSubmithandler" class="sendbutton"/>
+          </div>
           <Link to='/task/List'>
             <button>Back</button>
           </Link>
