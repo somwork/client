87e752de4c40a50ce914d010f29fceee249d1c47
kvartborg <hello@kvartborg.net>
2018-12-03 14:54:57 +0100

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './default.css'
-import budget from './api/budget'
-global.budget = budget
 
 const App = () => (
   <Routes />
