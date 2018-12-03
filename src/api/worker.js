2ffed8cd188dc81d73138eafa28d1cc62a7c06b3
MadsDueKristensen <32197042+MadsDueKristensen@users.noreply.github.com>
2018-12-03 16:14:03 +0100

    Fix

--- src/api/worker.js
+++ src/api/worker.js
@@ -56,6 +56,11 @@ export default {
     return res.ok
   },
 
+  /**
+   * Get all skills of worker with id
+   * @param {Number} id
+   * @return {Promise}
+   */
   async getSkills(id) {
     const res = await request.get(`workers/${id}/skills`)
     return await res.json()
