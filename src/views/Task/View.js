e9f76ce20a3814d618c584aadc37a3d134761ea8
Alex <32260519+alex6296@users.noreply.github.com>
2018-12-05 10:57:20 +0100

    Task detail view (#44)
    created view for a specifc task
    -made it possible to submit an estimate

--- src/views/Task/View.js
+++ src/views/Task/View.js
@@ -1,26 +1,88 @@
 import React, { Component } from "react";
 import Layout from '../../components/Layout';
 import Task from '../../api/task';
+import currencies from '../../api/currencies';
 import { Link } from 'react-router-dom';
+import Popup from "reactjs-popup";
+import'./TaskView.css'
+import moment from 'moment';
+import Alert from '../../components/Alert';
 
 export default class View extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = { tasks: [] }
-
-    this.loadTasks(this.props.match.params.id);
-  };
+    this.state={
+      task:{
+        id:'',
+        start:'',
+        deadline:'',
+        urgency:'',
+        description:'',
+        title:''
+      },
+      estimate:{
+        price:'',
+        totalHours:'',
+        currency:'',
+        complexity:'',
+      },
+      error:''
+    }
+  }
 
   /**
-   *loads all tasks from the db into the state
+   * Loads all tasks into state when componet mount
+   */
+  componentDidMount(){
+    this.loadtasks(this.props.match.params.id);
+  }
+
+  /**
+   *loads the selected task from the database into the state
    * @param {int} id
    */
+
   loadTasks = async id => {
     const res = await Task.get(id);
+    if(res=== null){
+      return null;
+    }
+    this.setState({task:res})
+  }
 
-    this.setState({ tasks: res })
+  /**
+   * event listener for input
+   * adds changes to this.state
+   * @param  {Object} event
+   */
+  changeHandler = event => {
+    const tempestimate = {...this.state.estimate }
+    tempestimate[event.target.name] = event.target.value
+    this.setState({
+        estimate:tempestimate
+    })
+  }
+
+  /**
+   * event listener for Submit
+   * validates all inputfields before sending a post request to the server
+   * @param {Object} event
+   */
+  submitHandler= async event =>{
+    event.preventDefault();
+    try {
+      await Task.createEstimate(this.state.task.id, {
+        price: Number(this.state.estimate.price),
+        totalHours: Number(this.state.estimate.totalHours),
+        currency: 'DKK',
+        complexity: Number(this.state.estimate.complexity),
+      });
+    } catch(err) {
+      this.setState({ error: err.message })
+    }
+
   }
 
   /**
@@ -28,33 +90,69 @@ export default class View extends Component {
    * @param {Object} task
    * @return {JSX} a task as a list item
    */
-  fieldRender(task) {
-    return (
-      <li key={task.id}>
-        <label>
-          <p><b>Task Id:</b> {task.id}</p>
-          <p>{task.start} → {task.deadline}</p>
-          <p>{task.urgencyString}</p>
+  fieldRendertaskDescription(task){
+    return(
+      <label key ={task.id}>
+        <div className="flex-container">
+          <div>
+            <b>published</b>
+            <div>
+              {moment(task.start).format('DD. MMM YYYY')}
+            </div>
+          </div>
+          <div>
+            <b>Deadline</b>
+            <div>
+            {moment(task.deadline).format('DD. MMM YYYY')}
+            </div>
+          </div>
+          <div>
+            <b>Urgency</b>
+            <div >
+              {task.urgency}
+            </div>
+          </div>
+        </div>
+        <div>
+          <hr/>
+          <h6>task Description:</h6>
           <p>{task.description}</p>
-        </label>
-      </li>
+        </div>
+      </label>
     )
   }
 
   /**
-   * Creates the Task overview view
+   * Creates the task overview view
    * @return {JSX} View
    */
   render() {
     return (
       <Layout>
         <section>
-          <h1>Task view</h1>
-          <ul>
-            {this.fieldRender(this.state.tasks)}
-          </ul>
-          <button>Make Offer</button>
-          <button>Chat</button>
+          <h1>{this.state.task.title}</h1>
+          <h3>task Details</h3>
+          {this.state.error && (
+            <Alert>{this.state.error}</Alert>
+          )}
+          <hr/>
+          {this.fieldRendertaskDescription(this.state.task)}
+          <Popup trigger={<button> Make estimate</button>}>
+            <div className='popUpInner'>
+              <label>
+                <form onSubmit={this.submitHandler}>
+                  <p><b>hourly pay:</b></p>
+                  <input name='price' type='number' onChange={this.changeHandler}  placeholder="Hourly pay..." required/>
+                  <p><b>total hours:</b></p>
+                  <input name='totalHours' type='number' onChange={this.changeHandler}  placeholder="Man Hours..." required/>
+                  <p> <b>Task Complexity:</b></p>
+                  <input name='complexity' type='number' onChange={this.changeHandler}  placeholder="Complexity..." required/>
+                  <input type="submit" value="Submit"/>
+                </form>
+              </label>
+            </div>
+          </Popup>
+          <hr/>
           <Link to='/task/List'>
             <button>Back</button>
           </Link>
